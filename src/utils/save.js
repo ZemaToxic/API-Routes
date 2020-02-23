ee81d85eb785e07398db7bc2afd3b18980490836
Crystal <crystal@zematoxic.com>
2020-02-24 11:40:19 +1300

    Attempt to include proper data

--- src/utils/save.js
+++ src/utils/save.js
@@ -2,7 +2,7 @@ module.exports = {
     save: async function(req, table, columns = '*') {
     
     const query = req.db._knex(table)
-    .insert({user: req.user, message: req.message})
+    .insert({user: req.body.user, message: req.body.message})
   
     return query
    }
