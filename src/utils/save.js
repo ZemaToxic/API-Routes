84763ee12f1ec8056fd1f552d81c2c13e549bb28
Crystal <crystal@zematoxic.com>
2020-02-24 11:14:12 +1300

    *whoops V2*

--- src/utils/save.js
+++ src/utils/save.js
@@ -1,9 +1,9 @@
 module.exports = {
     save: async function(req, table, columns = '*') {
     
-    const table = req.db._knex(table)
+    const query = req.db._knex(table)
     .insert({user: req.user, message: req.message})
   
-    return table
+    return query
    }
 } 
